423dbf53e22de965ab58275e4fb467452a2e6ed8
garima-2505 <112368687+garima-2505@users.noreply.github.com>
2024-05-23 13:03:00 +0530

    Update index.jsx

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"; 
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "server-main-production-0cf9.up.railway.app/",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
